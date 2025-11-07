3a29bdc4ff5b2df8632f14020394d271d81eaab6
Andrew Bates <358901+abates@users.noreply.github.com>
2025-11-07 16:43:37 -0500

    fix:Fixed issue with creating a new private key

--- plugins/action/tls_private_key.py
+++ plugins/action/tls_private_key.py
@@ -1,18 +1,23 @@
-#!/usr/bin/python
+"""Action plugin for creating or loading a TLS private key."""
 # Make coding more python3-ish, this is required for contributions to Ansible
-from __future__ import (absolute_import, division, print_function)
-__metaclass__ = type
 
 from ansible.plugins.action import ActionBase
+from ansible.utils.display import Display
+from ansible_collections.network_automation_labs.devops.plugins.module_utils.common import (
+    ActionPluginMixin,
+)
+from ansible_collections.network_automation_labs.devops.plugins.module_utils.crypto import (
+    CryptoPluginMixin,
+)
 
-from ansible_collections.network_automation_labs.devops.plugins.module_utils.common import ActionPluginMixin
-from ansible_collections.network_automation_labs.devops.plugins.module_utils.crypto import CryptoPluginMixin
+display = Display()
+
 
 class ActionModule(CryptoPluginMixin, ActionPluginMixin, ActionBase):
     def generate_new_key(self, task_vars, module_args):
-        result = {}
+        content = ""
         with self.tempfile(task_vars, module_args["path"]) as filename:
-            result.update(self.run_local_module(
+            result = self.run_local_module(
                 "community.crypto.openssl_privatekey",
                 task_vars,
                 path=filename,
@@ -20,9 +25,10 @@ class ActionModule(CryptoPluginMixin, ActionPluginMixin, ActionBase):
                 type=module_args["type"],
                 curve=module_args["curve"],
                 return_content=True,
-            ))
-            result["content"] = result["privatekey"]
-        return result
+            )
+            content = result["privatekey"]
+
+        return content
 
     def run(self, tmp=None, task_vars=None):
         if task_vars is None:
@@ -39,7 +45,9 @@ class ActionModule(CryptoPluginMixin, ActionPluginMixin, ActionBase):
                 "curve": {"type": "str", "required": True},
             },
         )
-        content, loaded = self.load_or_run(task_vars, module_args, module_args["path"], self.generate_new_key)
+        content, loaded = self.load_or_run(
+            task_vars, module_args, module_args["path"], self.generate_new_key
+        )
         result["changed"] = not loaded
         result["private_key_content"] = content
         return result
